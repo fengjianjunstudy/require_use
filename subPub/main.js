1616c60cc51011b2b15219a1b09a919eef06c598
fjj <fjj_321@126.com>
2015-11-27 14:49:25 +0800

    add topic

--- subPub/main.js
+++ subPub/main.js
@@ -32,8 +32,22 @@ requirejs(["app/subPub"],function(SubPub){
 				subPub.subscribe(nav.topic,nav.fn)
 				return nav;
 		}());
+	var detail=(function(){
+			var detail={
+				topic:"t2",
+				fn:function(data){
+					var oBody=document.getElementsByTagName("body")[0];
+					var oDiv=document.createElement("div");
+					oDiv.innerHTML=data["user_name"]+"的年龄："+data["age"]+"岁了";
+					oBody.appendChild(oDiv)
+				}
+			}
+			subPub.subscribe(detail.topic,detail.fn)
+			return detail;
+	}())
 
-	subPub.publish("t1",[{user_name:"xiaoming",age:18}])
+	//subPub.publish("t1",[{user_name:"xiaoming",age:18}])
 	subPub.unsubscribe("t1",header.fn);
 	subPub.publish("t1",[{user_name:"xiaoming",age:18}])
+	subPub.publish("t2",[{user_name:"xiaohua",age:20}])
 });
