3a21afa4d729a15e2d720043d3c59b49daddf7e6
coding笨妈 <fjj_321@126.com>
2015-12-04 16:42:07 +0800

    Delete main.js

--- yanzheng/js/main.js
+++
@@ -1,9 +0,0 @@
-define(["CheckValid"],function(CheckValid){ 
-	require.config({ 
-	});
-	document.getElementById("QQ").onchange=function(){ 
-		var type=this.data("type");
-		var 
-		CheckValid[type](this);
-	}
-})
